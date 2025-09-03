7e84c6446143c3ffb76f0be256adede085b77945
zmunro <zach@espresso.ai>
2025-09-02 20:01:31 -0400

    Update Homebrew formula to v2.0.4

--- Formula/outputbuddy.rb
+++ Formula/outputbuddy.rb
@@ -2,11 +2,10 @@
 class Outputbuddy < Formula
   desc "Flexible output redirection with color preservation"
   homepage "https://github.com/zmunro/outputbuddy"
-  version "2.0.0"
+  version "2.0.4"
 
-  # TODO: Update the URL after creating your first release
   url "https://github.com/zmunro/outputbuddy/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"  # This will be updated when you create the release
+  sha256 "3736f484e1fe9bec29cfaa2a1d2d84a7bfac0f27e57923c2bbf88ecba070baca"
 
   license "MIT"
 
